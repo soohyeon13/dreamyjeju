d89464196f5522b242e69d8410cc4c48162611ec
soohyeon13 <rlatngus6663@gmail.com>
2019-05-16 23:21:18 +0900

    httpsConnection

--- app/src/main/java/com/example/findpassword/RequestHttpsUrlConnection.java
+++ app/src/main/java/com/example/findpassword/RequestHttpsUrlConnection.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.List;
 
@@ -24,40 +25,60 @@ public class RequestHttpsUrlConnection {
 
     private final WebView myWebView;
     private final Activity activity;
-    private String url_Home,url_Login,url_Destination,host,referer,origin,body;
+    private String url_Home, url_Login, url_Destination, host, referer, origin, body;
 
     public RequestHttpsUrlConnection(WebView webView, Activity activity) {
         myWebView = webView;
         this.activity = activity;
     }
 
-    public void setUrl_Home(String url_home){this.url_Home = url_home;}
-    public void setUrl_Login(String url_login){this.url_Login=url_login;}
-    public void setUrl_Destination(String url_destination){this.url_Destination = url_destination;}
-    public void setHost(String host){this.host=host;}
-    public void setReferer(String referer){this.referer = referer;}
-    public void setOrigin(String origin){this.origin =origin;}
-    public void setBody(String body){this.body=body;}
-
-    public void execute(String id, String password) {
-        HttpsAsynTask httpsAsynTask = new HttpsAsynTask();
-        httpsAsynTask.execute(id,password);
+    public void setUrl_Home(String url_home) {
+        this.url_Home = url_home;
     }
 
-    private class HttpsAsynTask extends AsyncTask<String,Void,Void> {
+    public void setUrl_Login(String url_login) {
+        this.url_Login = url_login;
+    }
+
+    public void setUrl_Destination(String url_destination) {
+        this.url_Destination = url_destination;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setReferer(String referer) {
+        this.referer = referer;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void execute(String id, String password) {
+        HttpsAsycTask httpsAsycTask = new HttpsAsycTask();
+        httpsAsycTask.execute(id, password);
+    }
+
+    private class HttpsAsycTask extends AsyncTask<String, Void, Void> {
 
         private HttpURLConnection conn;
         private List<String> cookies;
 
         @Override
-        protected void  onPreExecute() {
+        protected void onPreExecute() {
             super.onPreExecute();
         }
 
         @Override
         protected Void doInBackground(String... strings) {
-            Log.d("ID","ID :" + strings[0]);
-            Log.d("Password","Password :" + strings[1]);
+            Log.d("ID", "ID :" + strings[0]);
+            Log.d("Password", "Password :" + strings[1]);
             try {
                 URL url = new URL(url_Home);
                 TrustAllHost trustAllHost = new TrustAllHost();
@@ -78,12 +99,11 @@ public class RequestHttpsUrlConnection {
 
                 cookies = conn.getHeaderFields().get("Set-Cookie");
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            finally {
+            } catch (MalformedURLException | ProtocolException exception) {
+                exception.printStackTrace();
+            } catch (IOException io) {
+                io.printStackTrace();
+            } finally {
                 conn.disconnect();
             }
 
@@ -111,15 +131,15 @@ public class RequestHttpsUrlConnection {
                 os.flush();
                 os.close();
 
-                if (conn.getResponseCode() != HttpsURLConnection.HTTP_OK){
+                if (conn.getResponseCode() != HttpsURLConnection.HTTP_OK) {
                     return null;
                 }
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }finally {
+            } catch (MalformedURLException | ProtocolException exception) {
+                exception.printStackTrace();
+            } catch (IOException io) {
+                io.printStackTrace();
+            } finally {
                 conn.disconnect();
             }
             return null;
@@ -134,13 +154,13 @@ public class RequestHttpsUrlConnection {
                 cookieManager.setAcceptCookie(true);
                 cookieManager.removeAllCookie();
 
-                for (String cookie :cookies) {
-                    cookieManager.setCookie(url_Destination,cookie);
+                for (String cookie : cookies) {
+                    cookieManager.setCookie(url_Destination, cookie);
                 }
 
                 CookieSyncManager.getInstance().sync();
                 myWebView.loadUrl(url_Destination);
-            }else{
+            } else {
                 AlertDialog dialog = null;
                 String loginFailedMassege = "로그인에 실패하셨습니다. \n다시 로그인 해주세요.";
 
@@ -149,7 +169,9 @@ public class RequestHttpsUrlConnection {
                 builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
 
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i) { activity.finish(); }
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        activity.finish();
+                    }
 
                 });
 
@@ -166,37 +188,38 @@ public class RequestHttpsUrlConnection {
             return loginCheck.equals("text/html;charset=UTF-8");
         }
 
-        private String Cookies(List<String>cookies){
-        String cookie ="";
+        private String Cookies(List<String> cookies) {
+            String cookie = "";
 
-        if (cookie != null) {
-            for (String temp_cookie : cookies) {
-                cookie += temp_cookie.split(";\\s*")[0] + "; ";
-                Log.d("@COOKIE", temp_cookie.split(";\\s*")[0]);
+            if (cookie != null) {
+                for (String temp_cookie : cookies) {
+                    cookie += temp_cookie.split(";\\s*")[0] + "; ";
+                    Log.d("@COOKIE", temp_cookie.split(";\\s*")[0]);
+                }
+
+                cookie = cookie.substring(0, cookie.length() - 1);
             }
 
-            cookie = cookie.substring(0,cookie.length()-1);
+            return cookie;
         }
 
-        return cookie;
-    }
-    private void RequestHeader() {
-        String cookie = Cookies(cookies);
+        private void RequestHeader() {
+            String cookie = Cookies(cookies);
 
-        conn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-        conn.setRequestProperty("Accept-Encoding", "gzip,deflate,br");
-        conn.setRequestProperty("Accept-Language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7");
-        conn.setRequestProperty("Cache-Control", "max-age=0");
-        conn.setRequestProperty("Cookie", cookie);
-        conn.setRequestProperty("Connection", "keep-alive");
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        conn.setRequestProperty("Host", host);
-        conn.setRequestProperty("Origin", origin);
-        conn.setRequestProperty("Referer", referer);
-        conn.setRequestProperty("Upgrade-Insecure-Requests", "1");
-        conn.setRequestProperty("User-Agent",
-                "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) "
-                        + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Mobile Safari/537.36");
+            conn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+            conn.setRequestProperty("Accept-Encoding", "gzip,deflate,br");
+            conn.setRequestProperty("Accept-Language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7");
+            conn.setRequestProperty("Cache-Control", "max-age=0");
+            conn.setRequestProperty("Cookie", cookie);
+            conn.setRequestProperty("Connection", "keep-alive");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Host", host);
+            conn.setRequestProperty("Origin", origin);
+            conn.setRequestProperty("Referer", referer);
+            conn.setRequestProperty("Upgrade-Insecure-Requests", "1");
+            conn.setRequestProperty("User-Agent",
+                    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) "
+                            + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Mobile Safari/537.36");
 
         }
     }
